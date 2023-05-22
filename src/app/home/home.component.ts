099047bb1c19d1a3bb132088ab81142e05c8d250
Deep Prakash Nishad <deep.prakash.nishad@gmail.com>
2023-05-23 00:04:50 +0530

    Second section added on homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -41,24 +41,22 @@ export class HomeComponent implements OnInit, AfterViewInit {
   ];
   slides: Slide[] = [
     {
-      headline: "For Your Current Mood",
-      src:
-        "https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+      headline: "Create donation landing page",
+      src: "/assets/images/carousel/medical.jpg",
+      text: "Need funds for Medical?",
+      callToActionText: "Create Campaign"
     },
     {
-      headline: "Miouw",
-      src:
-        "https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80"
+      headline: "Raising money has never been so easy",
+      src: "/assets/images/carousel/business.jpg",
+      text: "Need funds for Business?",
+      callToActionText: "Create Project"
     },
     {
-      headline: "In The Wilderness",
-      src:
-        "https://images.unsplash.com/photo-1557800634-7bf3c7305596?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2001&q=80"
-    },
-    {
-      headline: "Focus On The Writing",
-      src:
-        "https://images.unsplash.com/photo-1551410224-699683e15636?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80"
+      headline: "Scholarship for brilliant students",
+      src: "/assets/images/carousel/education.jpg",
+      text: "Need funds for Education?",
+      callToActionText: "Join Scholarship Program"
     }
   ];
 
